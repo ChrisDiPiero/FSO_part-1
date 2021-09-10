2d0a2db6a2d3669e4877316083c9ab6058e852e2
Chris DiPiero <frogmaster99@msn.com>
2021-09-10 15:28:01 -0500

    1.7 final

--- src/App.js
+++ src/App.js
@@ -14,66 +14,35 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const [all, setAll] = useState(1)
-  const [average, setAverage] = useState(0)
-  const [positive, setPositive] = useState(0)
+  const [all, setAll] = useState(0)
 
-  const update = (review) => {
-
-
-    switch (review) {
-      case 'good':
-        setGood(good + 1)
-        break
-      case 'neutral':
-        setNeutral(neutral + 1)
-        break
-      case 'bad':
-        setBad(bad + 1)
-        break
-      default:
-        return
-    }
-
-    calculateAverage()
-    calculatePositive()
+  const goodClick = () => {
+    setGood(good + 1)
+    setAll(all +1)
   }
-
-  const calculateAverage = () => {
-    console.log(all)
-    const calculate = (good - bad) / all 
-    setAverage( calculate )
+  const neutralClick = () => {
+    setNeutral(neutral + 1)
+    setAll(all +1)
   }
-
-  const calculatePositive = () => {
-    const calculate = good / all
-    setPositive( calculate )
+  const badClick = () => {
+    setBad(bad + 1)
+    setAll(all +1)
   }
-
-  // const setToGood = newValue => { setGoodCount(newValue) }
-  // const setToNeutral = newValue => {setNeutralCount(newValue) }
-  // const setToBad = newValue => { setBadCount(newValue) }
 
   return(
     <div>
       <h2>Give Feedback</h2>
-      <button onClick={ () => { setAll(all + 1) 
-                                update('good') } 
-                      }>good</button>
-      <button onClick={ () => { setAll(all + 1) 
-                                update('neutral') }              
-                        }>neutral</button>
-      <button onClick={ () => { setAll(all + 1)  
-                                update('bad') }
-                        }>bad</button>
+      <button onClick={goodClick}>good</button>
+      <button onClick={neutralClick}>neutral</button>
+      <button onClick={badClick}>bad</button>
 
       <h2>Statistics</h2>
       <p>Good: <Display value={good}/></p>
       <p>Neutral: <Display value={neutral}/></p>
       <p>Bad: <Display value={bad}/></p>
-      <p>All: <Display value={all - 1}/></p>
-      <p>Average: <Display value={average}/></p>
-      <p>Positive: <Display value={positive}/></p>
+      <p>All: <Display value={all}/></p>
+      <p>Average: <Display value={( good - bad ) / all}/></p>
+      <p>Positive: <Display value={good / all}/></p>
     </div>
   )
 }
